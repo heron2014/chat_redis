0803041556f59a85aeaa69d51dcb5f99fc9cdebd
Anita <a.nita@hotmail.co.uk>
2015-07-17 22:56:40 +0100

    appending new messages to the div#messages

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,19 @@
   console.log('testing');
   var socket = io();
   $('form').submit(function() {
-    var msg = $('#m').val();
+    var m = $('#m'),
+      msg = m.val();
     socket.emit('message', msg);
     console.log('Message from client ' + msg);
-    $('#m').val(''); //clear message after submit
+    m.val(''); //clear message after submit
     return false;
   });
 
   socket.on('message', function(msg) {
-    $('#messages').append($('li').text(msg));
+    $('<li></li>', {
+      text: msg
+    }).appendTo($('#messages'));
+    
   });
 
 })();
